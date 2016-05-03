dcb178fb937bc6b54b9b513130a7ade44dd80d87
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-05-02 23:40:34 -0300

    Fix some bugs

--- server.js
+++ server.js
@@ -34,15 +34,15 @@ http.createServer(function (req, res) {
                 console.log("Hi I'm the range now: "+ range);
             var partials = range.replace(/bytes=/, "").split("-");
             console.log(partials);
-            var start = parseInt(partials[0], 10);
-            var end = partials[1] ? parseInt(partials[1], 10) : total - 1;
-            var chunksize = (end-start)+1;
+            var start = parseInt(partials[0], 30);
+            var finish = partials[1] ? parseInt(partials[1], 30) : total - 1;
+            var position_size = (finish-start)+1;
 
-                res.writeHead(206, { "Content-Range": "bytes " + start + "-" + end + "/" + total, 
+                res.writeHead(206, { "Content-Range": "bytes " + start + "-" + finish + "/" + total, 
                                      "Accept-Ranges": "bytes",
-                                     "Content-Length": chunksize,
+                                     "Content-Length": position_size,
                                      "Content-Type":"video/mp4"});
-                res.end(current_movie.slice(start, end+1), "binary");
+                res.end(current_movie.slice(start, finish+1), "binary");
             }else{
                 res.writeHead(200, {"Content-Type":"video/mp4"} );
                 res.write(video_path, "binary");
